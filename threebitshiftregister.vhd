3ce81b670c9e55d6c96ee1924c17f0af76c844a0
JohnnyCanuck328 <91069350+JohnnyCanuck328@users.noreply.github.com>
2022-10-01 19:34:06 -0400

    added 8bit reg. ***all entities except enardFF_2 need to be tested

--- threebitshiftregister.vhd
+++ threebitshiftregister.vhd
@@ -25,19 +25,32 @@
 -- Prior permission is required to copy, republish, redistribute or post this work.
 -- This notice is adapted from the ACM copyright notice.
 --------------------------------------------------------------------------------
+
+--adapted from allaboutcircuits.com/textbook/digital/chpt-12/parallel-in-parallel-out-universal-shift-register/
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-ENTITY threeBitShiftRegister IS
+
+--notShiftDir is right shift
+--
+ENTITY eightBitUniversalReg IS
 	PORT(
 		i_resetBar, i_load	: IN	STD_LOGIC;
+		shiftDir, mode: in std_logic;
+		rightShiftIn, leftShiftIn: in std_logic;
 		i_clock			: IN	STD_LOGIC;
-		i_Value			: IN	STD_LOGIC;
-		o_Value			: OUT	STD_LOGIC);
-END threeBitShiftRegister;
+		i_Value			: IN	STD_LOGIC_VECTOR(7 downto 0);
+		o_Value			: OUT	STD_LOGIC_VECTOR(7 downto 0));
+END eightBitUniversalReg;
 
-ARCHITECTURE rtl OF threeBitShiftRegister IS
-	SIGNAL int_Value, int_notValue : STD_LOGIC_VECTOR(2 downto 0);
+ARCHITECTURE rtl OF eightBitUniversalReg IS
+	SIGNAL int_Value, int_notValue : STD_LOGIC_VECTOR(7 downto 0);
+	signal doShiftRight, doShiftLeft: std_logic_vector(7 downto 0);
+	signal load: std_logic_vector(7 downto 0);
+	signal NOTshiftDir, NOTmode: std_logic;
+	signal regIn: std_logic_vector(7 downto 0);
+	
 
 	COMPONENT enARdFF_2
 		PORT(
@@ -50,31 +63,114 @@ ARCHITECTURE rtl OF threeBitShiftRegister IS
 
 BEGIN
 
-msb: enARdFF_2
+--instaniate signals here
+NOTshiftDir <= NOT(shiftDir);
+NOTmode <= NOT(mode);
+
+--controls right shifts
+doShiftRight(7) <= mode AND NOTshiftDir AND rightShiftIn;
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(7);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(6);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(5);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(4);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(3);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(2);
+doShiftRight(7) <= mode AND NOTshiftDir AND int_Value(1);
+
+doShiftLeft(7) <= mode AND shiftDir AND int_Value(6);
+doShiftLeft(6) <= mode AND shiftDir AND int_Value(5);
+doShiftLeft(5) <= mode AND shiftDir AND int_Value(4);
+doShiftLeft(4) <= mode AND shiftDir AND int_Value(3);
+doShiftLeft(3) <= mode AND shiftDir AND int_Value(2);
+doShiftLeft(2) <= mode AND shiftDir AND int_Value(1);
+doShiftLeft(1) <= mode AND shiftDir AND int_Value(0);
+doShiftLeft(0) <= mode AND shiftDir AND leftShiftIn;
+
+load(7) <= NOTmode AND i_Value(7);
+load(6) <= NOTmode AND i_Value(6);
+load(5) <= NOTmode AND i_Value(5);
+load(4) <= NOTmode AND i_Value(4);
+load(3) <= NOTmode AND i_Value(3);
+load(2) <= NOTmode AND i_Value(2);
+load(1) <= NOTmode AND i_Value(1);
+load(0) <= NOTmode AND i_Value(0);
+
+regIn(7) <= load(7) OR doShiftRight(7) OR doShiftLeft(7);
+regIn(6) <= load(6) OR doShiftRight(6) OR doShiftLeft(6);
+regIn(5) <= load(5) OR doShiftRight(5) OR doShiftLeft(5);
+regIn(4) <= load(4) OR doShiftRight(4) OR doShiftLeft(4);
+regIn(3) <= load(3) OR doShiftRight(3) OR doShiftLeft(3);
+regIn(2) <= load(2) OR doShiftRight(2) OR doShiftLeft(2);
+regIn(1) <= load(1) OR doShiftRight(1) OR doShiftLeft(1);
+regIn(0) <= load(0) OR doShiftRight(0) OR doShiftLeft(0);
+
+
+ff7: enARdFF_2
 	PORT MAP (i_resetBar => i_resetBar,
-			  i_d => i_Value, 
+			  i_d => regIn(7), 
+			  i_enable => i_load,
+			  i_clock => i_clock,
+			  o_q => int_Value(7),
+	          o_qBar => int_notValue(7));
+
+ff6: enARdFF_2
+	PORT MAP (i_resetBar => i_resetBar,
+			  i_d => regIn(6),
+			  i_enable => i_load, 
+			  i_clock => i_clock,
+			  o_q => int_Value(6),
+	          o_qBar => int_notValue(6));
+
+ff5: enARdFF_2
+	PORT MAP (i_resetBar => i_resetBar,
+			  i_d => regIn(5), 
+			  i_enable => i_load,
+			  i_clock => i_clock,
+			  o_q => int_Value(5),
+	          o_qBar => int_notValue(5));
+				 
+ff4: enARdFF_2
+	PORT MAP (i_resetBar => i_resetBar,
+			  i_d => regIn(4), 
+			  i_enable => i_load,
+			  i_clock => i_clock,
+			  o_q => int_Value(4),
+	          o_qBar => int_notValue(4));
+
+ff3: enARdFF_2
+	PORT MAP (i_resetBar => i_resetBar,
+			  i_d => regIn(3),
+			  i_enable => i_load, 
+			  i_clock => i_clock,
+			  o_q => int_Value(3),
+	          o_qBar => int_notValue(3));
+
+ff2: enARdFF_2
+	PORT MAP (i_resetBar => i_resetBar,
+			  i_d => regIn(2), 
 			  i_enable => i_load,
 			  i_clock => i_clock,
 			  o_q => int_Value(2),
 	          o_qBar => int_notValue(2));
 
-ssb: enARdFF_2
+ff1: enARdFF_2
 	PORT MAP (i_resetBar => i_resetBar,
-			  i_d => int_Value(2),
-			  i_enable => i_load, 
+			  i_d => regIn(1), 
+			  i_enable => i_load,
 			  i_clock => i_clock,
 			  o_q => int_Value(1),
 	          o_qBar => int_notValue(1));
 
-lsb: enARdFF_2
+ff0: enARdFF_2
 	PORT MAP (i_resetBar => i_resetBar,
-			  i_d => int_Value(1), 
-			  i_enable => i_load,
+			  i_d => regIn(0),
+			  i_enable => i_load, 
 			  i_clock => i_clock,
 			  o_q => int_Value(0),
 	          o_qBar => int_notValue(0));
+			 
 
 	-- Output Driver
-	o_Value		<= int_Value(0);
+	o_Value		<= int_Value;
 
 END rtl;
